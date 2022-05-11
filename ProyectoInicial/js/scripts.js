66472895d3b106a765bc61c885ceaab48534c440
Lucasoviedo <lucasoviedolucas@gmail.com>
2022-05-11 17:31:53 -0300

    Primer commit

---
+++ ProyectoInicial/js/scripts.js
@@ -0,0 +1,20 @@
+/*
+
+var acc = document.getElementsByClassName("accordionButton");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    var accordionBody = this.nextElementSibling;
+    if (accordionBody.style.display === "block") {
+        accordionBody.style.display = "none";
+    } else {
+        accordionBody.style.display = "block";
+    }
+  });
+}
+
+*/
